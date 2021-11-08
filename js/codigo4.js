bd1c5abdb8cd580a9a591abfe029cb3e761c6efb
iei132 <93917261+iei132@users.noreply.github.com>
2021-11-08 06:24:11 -0500

    Add files via upload

---
+++ js/codigo4.js
@@ -0,0 +1,74 @@
+function cambiarClase() {
+    let navbar = document.getElementById('navbar');
+        navbar.classList.toggle('navbar-open');
+
+}
+
+
+const gallery  = document.querySelectorAll(".image"),
+previewBox = document.querySelector(".preview-box"),
+previewImg = previewBox.querySelector("img"),
+closeIcon = previewBox.querySelector(".icon"),
+currentseleccion = previewBox.querySelector(".current-seleccion"),
+totalseleccion = previewBox.querySelector(".total-seleccion"),
+shadow = document.querySelector(".shadow");
+
+window.onload = ()=>{
+    for (let i = 0; i < gallery.length; i++) {
+        totalseleccion.textContent = gallery.length; 
+        let newIndex = i; 
+        let clickedImgIndex; 
+        
+        gallery[i].onclick = () =>{
+            clickedImgIndex = i; 
+            function preview(){
+                currentseleccion.textContent = newIndex + 1; 
+                let imageURL = gallery[newIndex].querySelector("img").src; 
+                previewImg.src = imageURL; 
+            }
+            preview(); 
+    
+            const prevBtn = document.querySelector(".prev");
+            const nextBtn = document.querySelector(".next");
+            if(newIndex == 0){ 
+                prevBtn.style.display = "none"; 
+            }
+            if(newIndex >= gallery.length - 1){ 
+                nextBtn.style.display = "none"; 
+            }
+            prevBtn.onclick = ()=>{ 
+                newIndex--; 
+                if(newIndex == 0){
+                    preview(); 
+                    prevBtn.style.display = "none"; 
+                }else{
+                    preview();
+                    nextBtn.style.display = "block";
+                } 
+            }
+            nextBtn.onclick = ()=>{ 
+                newIndex++; 
+                if(newIndex >= gallery.length - 1){
+                    preview(); 
+                    nextBtn.style.display = "none";
+                }else{
+                    preview(); 
+                    prevBtn.style.display = "block";
+                }
+            }
+            document.querySelector(".portfolio").style.overflow = "hidden";
+            previewBox.classList.add("show"); 
+            shadow.style.display = "block"; 
+            closeIcon.onclick = ()=>{
+                newIndex = clickedImgIndex; 
+                prevBtn.style.display = "block"; 
+                nextBtn.style.display = "block";
+                previewBox.classList.remove("show");
+                shadow.style.display = "none";
+                document.querySelector(".portfolio").style.overflow = "scroll";
+            }
+        }
+        
+    }
+    
+}
